e3993bcf8a23c91160c52c29f920bc1faa59cea3
d-bahr <d-bahr@users.noreply.github.com>
2026-06-02 05:08:01 +0000

    deploy: eee37c92b77ef2f420832c201cfec15fb0418347

--- navtreedata.js
+++ navtreedata.js
@@ -58,6 +58,6 @@ var NAVTREEINDEX =
 "_c_r_c_8h.html"
 ];
 
-var SYNCONMSG = 'click to disable panel synchronization';
-var SYNCOFFMSG = 'click to enable panel synchronization';
-var LISTOFALLMEMBERS = 'List of all members';
+const SYNCONMSG = 'click to disable panel synchronization';
+const SYNCOFFMSG = 'click to enable panel synchronization';
+const LISTOFALLMEMBERS = 'List of all members';
